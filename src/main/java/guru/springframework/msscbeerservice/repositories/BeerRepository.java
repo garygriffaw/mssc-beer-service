6c7aebce043365a774de4cdb99ee5de086fb218a
Gary Griffaw <ggriffaw@yahoo.com>
2023-06-08 11:53:40 -0500

    Add get beer inventory, listBeers, and data loader

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,10 +1,19 @@
 package guru.springframework.msscbeerservice.repositories;
 
 import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
 }
